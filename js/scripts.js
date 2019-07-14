a486659af95f90e8b0b6e742cb32e1a042bc89ec
Maria Mazur <maria.mazur@creditshelf.com>
2019-07-14 10:38:53 +0200

    Style input field

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,10 @@
   window.onload = init;
 
   function init() {
-    var city = document.getElementById('location').textContent || 'Mainz';
+    var city = document.getElementById('autoComplete').textContent || 'Mainz';
     getWeatherFromApi(city);
-    setCurrentTime();
-    setInterval(setCurrentTime, 1000);
     setInterval(setCity, 1000);
   }
-
 })(window, document, undefined);
 
 // api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=18ceef033fcda8f9e022df00c39956c9
@@ -21,7 +18,6 @@ function getWeatherFromApi(city) {
   var request = new XMLHttpRequest()
   var apiCall = `https://${WEATHER_API}/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
 
-  // Open a new connection, using the GET request on the URL endpoint
   request.open('GET', apiCall, true);
 
   request.onload = function () {
@@ -29,25 +25,39 @@ function getWeatherFromApi(city) {
     if (request.status >= 200 && request.status < 400) {
       document.getElementById('city-weather').textContent = data.weather[0].main;
       document.getElementById('city-temperature').textContent = `${convertKelvinToCelsius(data.main.temp)} °C`;
+      document.getElementById('city-time').textContent = convertDate(data.dt);
     } else {
       console.log('Opps, something went wrong!')
     }
   }
-  request.send(); // Send request
+  request.send(); 
 };
 
 function convertKelvinToCelsius(kelvin) {
   ZERO = 273.15;
   return kelvin < (0) ?  'below absolute zero (0 K)' : (kelvin - ZERO).toFixed(1);
-  
 }
 
-function setCurrentTime() {
-  document.getElementById('city-time').textContent 
-    = new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+function convertDate(date){
+  return new Date(date * 1000).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 }
 
 function setCity() {
-  var city = document.getElementById('location').value || 'Mainz';
-  document.getElementById('city-name').textContent = city;
+  let typingTimer;
+  let doneTypingInterval = 1000;
+  let cityInput = document.getElementById('autoComplete');
+
+  cityInput.addEventListener('keyup', () => {
+      clearTimeout(typingTimer);
+      if (cityInput.value) {
+          typingTimer = setTimeout(doneTyping, doneTypingInterval);
+      }
+  });
+
+  function doneTyping () {
+    let city = cityInput.value || 'Mainz';
+    document.getElementById('city-name').textContent = city;
+    getWeatherFromApi(city);
+  }
+  
 }
